765a8ae292ed310c31ec8d2a1ecb17db73e5b8e8
grisa <grisa@mail.ru>
2022-02-13 00:27:35 +0300

    Rock paper scissors complete and playable

--- main.js
+++ main.js
@@ -1,56 +1,84 @@
 let plays = ['rock', 'paper', 'scissors'];
 let para = document.querySelector('p');
 let btn = document.getElementById('play-button');
+const gameDiv = document.getElementById('game-div');
+const textDiv = document.getElementById('text-div');
+const scoreBoard = document.getElementById('score-board')
+let playerScore = 0;
+let computerScore = 0;
 
-// document.addEventListener('click', btn, playRound(computerPlay()));
+btn.addEventListener('click', playGame)
+
+function playGame() {
+    btn.remove();
+    let score = document.createElement('h2');
+    score.innerHTML = `Player Score: ${playerScore} Computer Score: ${computerScore}`;
+    scoreBoard.appendChild(score);
+    createButtons();
+}
+
+function createButtons() {
+    const btnRock = document.createElement('button');
+    const btnPaper = document.createElement('button');
+    const btnScissors = document.createElement('button');
+    const buttons = [btnRock, btnPaper, btnScissors];
+    addButtonStyling(buttons);
+    btnRock.textContent = 'Rock';
+    btnPaper.textContent = 'Paper';
+    btnScissors.textContent = 'Scissors';
+    buttons.forEach(button => {
+        addButtonFunctionality(button);
+        gameDiv.appendChild(button);
+    });
+}
+
+function addButtonStyling (buttons) {
+    buttons.forEach(button => {
+        button.classList.add('button-1');
+    });
+}
+
+function addButtonFunctionality (button) {
+    button.addEventListener('click', () => {
+        playRound(button.textContent.toLowerCase(), computerPlay());
+    })
+}
+
+function playRound(playerSelection, computerSelection) {
+    let newh3;
+    if (textDiv.querySelector('h3')) { // check if there is an h3 element in the gameDiv, if not create one
+        newh3 = textDiv.querySelector('h3');
+    } else {
+        newh3 = document.createElement('h3');
+        textDiv.appendChild(newh3);
+    }
+    if (playerSelection === computerSelection) {
+        newh3.innerHTML = `You chose: ${playerSelection} - Computer: ${computerSelection}<br>It's a draw!`;
+    }else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') || 
+        (playerSelection === 'scissors' && computerSelection === 'paper') || 
+        (playerSelection === 'paper' && computerSelection === 'rock')) {
+            playerScore++;
+            updateScoreBoard();
+            newh3.innerHTML = `You chose: ${playerSelection} - Computer: ${computerSelection}<br>Player won!`;
+    }else {
+        computerScore++;
+        updateScoreBoard();
+        newh3.innerHTML = `You chose: ${playerSelection} - Computer: ${computerSelection}<br>Computer won, you lost!`;
+    }
+    
+}
+
+function updateScoreBoard () {
+    let score = scoreBoard.querySelector('h2');
+    score.innerHTML = `Player Score: ${playerScore} Computer Score: ${computerScore}`;
+}
 
 function computerPlay() {
     return plays[Math.floor(Math.random() * plays.length)];
 }
 
-function playRound(computerSelection) {
-    let playerSelection = prompt('Pick your weapon! Rock, Paper, or Scissors?').trim().toLowerCase();
-    if (playerSelection === computerSelection) {
-        console.log(playerSelection, computerSelection)
-        console.log('Its a draw!');
-    }else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
-        console.log(playerSelection, computerSelection)
-        console.log('Player won!');
-        return 'player';
-    }else {
-        console.log(playerSelection, computerSelection)
-        console.log('Computer won!')
-        return 'computer';
-    }
-}
 
 
-function playGame() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let draw = 0;
-    for (let i = 0; i < 5; i++){
-        let game = playRound(computerPlay());
-        if (game === 'player') {
-            playerScore++;
-        } else if (game === 'computer') {
-            computerScore++;
-        }
-    }
-    switch (true) {
-        case (playerScore > computerScore):
-            console.log(playerScore, computerScore);
-            para.textContent = `Player won! Congradulations. The final score is player: ${playerScore}, computer: ${computerScore}`;
-            break;
-        case (playerScore < computerScore):
-            para.textContent = `Computer won! The final score is player: ${playerScore}, computer: ${computerScore}`;
-            break
-        case (playerScore === computerScore):
-            console.log(playerScore, computerScore);
-            para.textContent = `Its a draw! Incredible`
-    }
-}
 
-
-playGame();
 
